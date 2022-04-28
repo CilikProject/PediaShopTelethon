73f29226a57e956e4996d4b45fcd1c861cb3ea4c
PayXr <103725749+PayXr@users.noreply.github.com>
2022-04-28 03:31:05 -0700

    .

--- CilikUbot/modules/sql_helper/snips_sql.py
+++ CilikUbot/modules/sql_helper/snips_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Numeric, UnicodeText
 
-from userbot.modules.sql_helper import BASE, SESSION
+from CilikUbot.modules.sql_helper import BASE, SESSION
 
 
 class Note(BASE):
