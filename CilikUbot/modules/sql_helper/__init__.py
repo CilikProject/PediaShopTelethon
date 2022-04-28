60ee608911d16144ba93a06eda696df553b026eb
PayXr <103725749+PayXr@users.noreply.github.com>
2022-04-28 03:21:09 -0700

    .

--- CilikUbot/modules/sql_helper/__init__.py
+++ CilikUbot/modules/sql_helper/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from userbot import DB_URI
+from CilikUbot import DB_URI
 
 if DB_URI.startswith("postgres://"):
     uri = DB_URI.replace("postgres://", "postgresql://", 1)
