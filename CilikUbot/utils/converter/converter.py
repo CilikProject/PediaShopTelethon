971ca652648ca766ef7a026f0adb79311e79ed89
PayXr <103725749+PayXr@users.noreply.github.com>
2022-04-28 02:35:14 -0700

    .

--- CilikUbot/utils/converter/converter.py
+++ CilikUbot/utils/converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from userbot.utils.exceptions import FFmpegReturnCodeError
+from CilikUbot.utils.exceptions import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
