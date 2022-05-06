ce70c1f00c22d4557563f351f71be9b6be44d0fc
PayXr <103725749+PayXr@users.noreply.github.com>
2022-05-06 04:11:58 -0700

    rewrite

--- CilikUbot/__main__.py
+++ CilikUbot/__main__.py
@@ -7,36 +7,44 @@
 
 import sys
 from importlib import import_module
-from pytgcalls import idle
+from platform import python_version
 
-from CilikUbot import (
-    BOT_VER,
-    LOGS,
-    bot,
-)
-from CilikUbot.modules import ALL_MODULES
+from pytgcalls import __version__ as pytgcalls
+from pytgcalls import idle
+from telethon import version
+
+from CilikUbot import BOT_TOKEN
+from CilikUbot import BOT_VER as ubotversion
+from CilikUbot import BOTLOG_CHATID, LOGS, LOOP, bot
 from CilikUbot.clients import cilik_userbot_on, multicilik
 from CilikUbot.core.git import git
+from CilikUbot.modules import ALL_MODULES
+
 
 try:
+    for module_name in ALL_MODULES:
+        imported_module = import_module(f"CilikUbot.modules.{module_name}")
     client = multicilik()
     total = 10 - client
     git()
     LOGS.info(f"Total Clients = {total} User")
-except Exception as e:
+    LOGS.info(f"Python Version - {python_version()}")
+    LOGS.info(f"Telethon Version - {version.__version__}")
+    LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
+    LOGS.info(f"Cilik-Ubot Version - {ubotversion} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
+    pass
+except BaseException as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
 
-for module_name in ALL_MODULES:
-    imported_module = import_module("CilikUbot.modules." + module_name)
 
-bot.loop.run_until_complete(cilik_userbot_on())
-LOGS.info(
-    f"Jika Anda Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/CilikSupport")
-LOGS.info(
-    f"✨Cilik-Ubot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+LOOP.run_until_complete(cilik_userbot_on())
 idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
-    bot.run_until_disconnected()
+    try:
+        bot.run_until_disconnected()
+    except ConnectionError:
+        pass
