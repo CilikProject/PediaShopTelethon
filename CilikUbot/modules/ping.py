7d6a7b71edcd1134d5ac1dd6828ac74584762db5
PayXr <103725749+PayXr@users.noreply.github.com>
2022-05-08 03:43:39 +0700

    Update ping.py

--- CilikUbot/modules/ping.py
+++ CilikUbot/modules/ping.py
@@ -32,11 +32,11 @@ async def get_readable_time(seconds: int) -> str:
 @cilik_cmd(pattern="ping$")
 async def _(pang):
     """For .ping command, ping the userbot from any chat."""
-    await get_readable_time((time.time() - StartTime))
+    uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    xx = await pang.reply("...")
+    xx = await pang.reply("**...**")
     end = datetime.now()
-    (end - start).microseconds / 1000
+    duration = (end - start).microseconds / 1000
     await xx.edit(
-        f"**Pong!**\n`%sms`"
+        f"**Pong** \n`%sms`" % (duration)
     ) 
