7cba43c7aeb664ebbfba4d2796f41b47488d8e7d
PayXr <103725749+PayXr@users.noreply.github.com>
2022-05-08 04:33:44 +0700

    .

--- CilikUbot/modules/ping.py
+++ CilikUbot/modules/ping.py
@@ -34,9 +34,9 @@ async def _(pang):
     """For .ping command, ping the userbot from any chat."""
     await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    xx = await pang.reply("**...**")
+    xx = await pang.reply("...")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await xx.edit(
-        f"**Pong** \n`%sms`" % (duration)
+        f"**Pong!** \n`%sms`" % (duration)
     ) 
