fefb1f1732a95b7ff7ea858766a11e63804016e8
PayXr <103725749+PayXr@users.noreply.github.com>
2022-12-16 23:47:21 +0700

    Update decorators.py

--- CilikUbot/core/decorators.py
+++ CilikUbot/core/decorators.py
@@ -5,14 +5,21 @@ from CilikUbot import SUDO_USERS, bot, owner, sender
 from CilikUbot.modules.sql_helper.globals import gvarstatus
 from CilikUbot import bot
 
+import asyncio
+
+from telethon.errors import FloodWaitError, MessageNotModifiedError
+from telethon.events import CallbackQuery
+
+from CilikUbot import sender, bot, owner
+from CilikUbot.modules.sql_helper.globals import gvarstatus
+
+
 def check_owner(func):
     async def wrapper(c_q: CallbackQuery):
         user = await bot.get_me()
-        user = await bot.get_sender()
         uid = user.id
         if c_q.query.user_id and (
             c_q.query.user_id == uid or c_q.query.user_id in sender
-            c_q.query.user_id == uid 
         ):
             try:
                 await func(c_q)
