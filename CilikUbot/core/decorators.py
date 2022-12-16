0b60c9f1e75aff92ccf3d2c1c839b1f0c8956633
PayXr <103725749+PayXr@users.noreply.github.com>
2022-12-17 00:54:25 +0700

    Update decorators.py

--- CilikUbot/core/decorators.py
+++ CilikUbot/core/decorators.py
@@ -1,25 +1,18 @@
-from telethon.errors import FloodWaitError, MessageNotModifiedError
-from telethon.events import CallbackQuery
-
-from CilikUbot import bot
-from CilikUbot.modules.sql_helper.globals import gvarstatus
-from CilikUbot import bot
-
 import asyncio
 
 from telethon.errors import FloodWaitError, MessageNotModifiedError
 from telethon.events import CallbackQuery
 
-from CilikUbot import bot
+from CilikUbot import SUDO_USERS, bot, owner
 from CilikUbot.modules.sql_helper.globals import gvarstatus
 
 
 def check_owner(func):
     async def wrapper(c_q: CallbackQuery):
-        user = await bot.get_me() or await bot.get_sender()
+        user = await bot.get_me()
         uid = user.id
         if c_q.query.user_id and (
-            c_q.query.user_id == uid 
+            c_q.query.user_id == uid or c_q.query.user_id in SUDO_USERS
         ):
             try:
                 await func(c_q)
@@ -30,7 +23,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or f"Hanya bos saya yang dapat Mengakses Ini!!\nSilahkan Buat UserBot Anda sendiri."
+                or f"Hanya {owner} yang dapat Mengakses Ini!!\nSilahkan Buat UserBot Anda sendiri."
             )
             await c_q.answer(
                 HELP_TEXT,
