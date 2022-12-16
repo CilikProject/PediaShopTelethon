be05eec53970ed13bf1e5f4d62c50daebea6e16d
PayXr <103725749+PayXr@users.noreply.github.com>
2022-12-17 03:26:09 +0700

    Update __main__.py

--- CilikUbot/__main__.py
+++ CilikUbot/__main__.py
@@ -7,7 +7,6 @@
 
 import sys
 from importlib import import_module
-from pytgcalls import idle
 
 from CilikUbot import (
     BOT_VER,
@@ -19,23 +18,27 @@ from CilikUbot.clients import cilik_userbot_on, multicilik
 from CilikUbot.core.git import git
 
 try:
+    for module_name in ALL_MODULES:
+        imported_module = import_module(f"CilikUbot.modules.{module_name}")
     client = multicilik()
     total = 20 - client
     git()
-    LOGS.info(f"Total Clients = {total} User")
-except Exception as e:
+    LOGS.info(f"Total Clients = {total} User"))
+    LOGS.info("[✨ BERHASIL DIAKTIFKAN! ✨]")
+except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
+    pass
+except BaseException as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
 
-for module_name in ALL_MODULES:
-    imported_module = import_module("CilikUbot.modules." + module_name)
+
 
 bot.loop.run_until_complete(cilik_userbot_on())
 LOGS.info(
     f"Jika Anda Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/CilikSupport")
 LOGS.info(
     f"✨Cilik-Ubot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
-idle()
+
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
